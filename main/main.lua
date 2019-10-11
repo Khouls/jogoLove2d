88a6d866f29c59839c0309541120554788d5f984
guilherme.flach <guilherme.flach@FELIZ.INTERNO>
2019-10-11 09:10:06 -0600

    Parallax Scroll implementado

--- main/main.lua
+++ main/main.lua
@@ -4,13 +4,27 @@ function love.load()
 
 	love.window.setMode(960,640,nil)
 
+
+	math.randomseed(os.time())
+	math.random()
+	math.random()
+	math.random()
+
+	tree = love.graphics.newImage("tree.png")
+	treeW, treeH = tree:getDimensions()
+	img = love.graphics.newImage("maxresdefault.jpg")
+
+
+
 	--cria a função de camera
 	camera = {}
+		camera.layers = {}
 		camera.x = 0
 		camera.y = 0
 		camera.scaleX = 1
 		camera.scaleY = 1
 		camera.rotation = 0
+
 
 		function camera:set()
 		  love.graphics.push()
@@ -46,6 +60,24 @@ function love.load()
 		function camera:setScale(sx, sy)
 		  self.scaleX = sx or self.scaleX
 		  self.scaleY = sy or self.scaleY
+		end
+
+		function camera:newLayer(scale, func)
+	  		table.insert(self.layers, { draw = func, scale = scale })
+  			table.sort(self.layers, function(a, b) return a.scale < b.scale end)
+		end
+
+		function camera:draw()
+		 local bx, by = self.x, self.y
+		  for _, v in ipairs(self.layers) do
+		    self.x = bx * v.scale
+		    self.y = by * v.scale
+				camera:set()
+				v.draw()
+				camera:unset()
+
+		  end
+
 		end
 
 	--finalização da câmera
@@ -87,14 +119,63 @@ function love.load()
 	terreno = {} --array contendo todos os terrenos
 
 		terreno.chao = {}
-		terreno.chao.body = love.physics.newBody(mundo, w/2, h/2 + 50, "static")
-		terreno.chao.shape = love.physics.newRectangleShape(w/2, 5)
+		terreno.chao.body = love.physics.newBody(mundo, w/2, h+500, "static")
+		terreno.chao.shape = love.physics.newRectangleShape(w/2, 2*h)
 		terreno.chao.fixture = love.physics.newFixture(terreno.chao.body, terreno.chao.shape)
 
 
+
+	--layer 1 vai ser do player e do terreno
+
+	--desenha player e terreno
+	camera:newLayer(1, function()
+		love.graphics.polygon("fill", player.body:getWorldPoints(player.shape:getPoints()))
+		for i,v in pairs(terreno) do
+			love.graphics.polygon("fill", v.body:getWorldPoints(v.shape:getPoints()))
+		end
+   	 	love.graphics.print(debugText)
+
+	end)
+
+	--desenha o fundo
+	camera:newLayer(0.3, function()
+		love.graphics.setColor(1,1,1,1)
+		love.graphics.draw(img, -100, -100)
+
+	end)
+
+	--desenha as árvores da frente
+	posArvores = {}
+	for i=-600,600,(treeW*0.8+50) do
+		ar = {}
+
+		for j=-1,3 do
+			table.insert(ar,-i)
+		end
+
+		table.insert(posArvores, ar)
+	end
+
+
+	for i,v in ipairs(posArvores) do
+		camera:newLayer(i+0.5, function()
+			for k,v1 in ipairs(v) do
+				love.graphics.draw(tree, v1+500*(k-1), 100, 0, 0.8,0.8)
+			end
+		end)
+	end
 end
 
+
 function love.update(dt)
+	--out of bounds
+	if player.y >= 10000 or player.x <= -10000 then
+		print("oof")
+		player.body:setPosition(w/2,h/2)
+	end
+
+  	camera:setPosition(player.x, player.y)
+  	camera:move(-w/2, -h/2)
 	
 
 	mundo:update(dt)
@@ -135,24 +216,18 @@ function love.keypressed(key)
 		player.onPlataform = false	
 	end
 
-	 if key == "escape" then love.event.push("quit") end --fecha o programa se apertar esc
+	 
+	if key == "'" then
+		love.load()
+	elseif key == 'escape' then
+		love.event.push('q')
+	end
 
 end
 
 function love.draw()
-    camera:set()
+	camera:draw()
 
-    camera:setPosition(player.x, player.y)
-    camera:move(-w/2, -h/2)
-
-    love.graphics.print(debugText)
-
-	love.graphics.polygon("fill", player.body:getWorldPoints(player.shape:getPoints()))
-
-	for i,v in pairs(terreno) do
-		love.graphics.polygon("fill", v.body:getWorldPoints(v.shape:getPoints()))
-	end
-	camera:unset()
 end
 
 function beginContact(obj1, obj2, collObj)
